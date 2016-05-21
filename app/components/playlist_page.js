57c40ea53f7f5d9fe733676b1291111d986fb8ab
msmykowski <smycal@gmail.com>
2016-05-21 14:38:40 -0700

    Send playlistUpdate event to server using value form input when clicking the add entry button

--- app/components/playlist_page.js
+++ app/components/playlist_page.js
@@ -1,15 +1,31 @@
 const React = require('react');
 const types = require('react').PropTypes;
+const {Actions} = require('p-flux');
 
 class PlaylistPage extends React.Component {
   static propTypes = {
-    entries: types.array
+    playlist: types.object
   };
 
+  onClick = () => {
+    const addedEntry = this._input.value;
+    const {playlist={}} = this.props;
+    const {entries} = playlist;
+    Actions.playlistUpdate({...playlist, entries: [...entries, addedEntry]});
+  }
+
   render() {
-    const {entries} = this.props;
+    const {playlist: {entries}={}} = this.props;
     const entriesList = entries && entries.map((entry, i) => <div className="entry" key={i}>{entry}</div>);
-    return (<div className="entries-list">{entriesList}</div>);
+    return (
+      <div>
+        <input name="entry-input" className="entry-input" ref={(c) => this._input = c}/>
+        <button className="add-entry" onClick={this.onClick}>Add Entry</button>
+        <div className="entries-list">
+          {entriesList}
+        </div>
+      </div>
+    );
   }
 }
 
--- spec/app/components/playlist_page_spec.js
+++ spec/app/components/playlist_page_spec.js
@@ -1,9 +1,11 @@
 require('../spec_helper');
 
 describe('PlaylistPage', () => {
+  let playlist, entries;
   beforeEach(() => {
-    const entries = ['song1', 'song2'];
-    const props = {entries};
+    entries = ['song1', 'song2'];
+    playlist = {id: 1, entries};
+    const props = {playlist};
     const PlaylistPage = require('../../../app/components/playlist_page');
     ReactDOM.render(<PlaylistPage {...props}/>, root);
   });
@@ -12,5 +14,18 @@ describe('PlaylistPage', () => {
     expect('.entries-list').toExist();
     expect('.entry:eq(0)').toContainText('song1');
     expect('.entry:eq(1)').toContainText('song2');
+  });
+
+  describe('clicking the add song', () => {
+    let newSong;
+    beforeEach(() => {
+      newSong = 'song3';
+      $('.entry-input').val(newSong);
+      $('.add-entry').simulate('click');
+    });
+
+    it('dispatches a playlist update event', () => {
+      expect(Dispatcher.dispatch).toHaveBeenCalledWith({type: 'playlistUpdate', data: {...playlist, entries: [...entries, newSong]}});
+    });
   });
 });
